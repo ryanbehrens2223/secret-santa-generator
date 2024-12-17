6ec800f3dbf3352357c72e6b5856403ccfba7b48
Ryan Behrens <ryanbehrens23@gmail.com>
2024-12-16 19:19:11 -0600

    Update browser tab

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { Container, Box, Typography, List, ListItem, ListItemText } from "@mui/material";
 import SecretSantaForm from "./components/SecretSantaForm";
 import { generateSecretSantaPairs } from "./services/secretSantaServices";
+import Head from "next/head";
 
 export default function Home() {
   const [participants, setParticipants] = useState<string[]>([]);
@@ -16,31 +17,37 @@ export default function Home() {
   };
 
   return (
-    <Container maxWidth="lg" sx={{ minHeight: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', py: 4 }}>
-      <Typography variant="h3" component="h1" gutterBottom>
-        Secret Santa Generator
-      </Typography>
-      <Box sx={{ display: 'flex', width: '100%', mt: 4 }}>
-        <Box sx={{ flex: 1, mr: 2 }}>
-          <SecretSantaForm onSubmit={handleFormSubmit} />
+    <>
+      <Head>
+        <title>Secret Santa Generator</title>
+        <meta name="description" content="Generate Secret Santa pairs" />
+      </Head>
+      <Container maxWidth="lg" sx={{ minHeight: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', py: 4 }}>
+        <Typography variant="h3" component="h1" gutterBottom>
+          Secret Santa Generator
+        </Typography>
+        <Box sx={{ display: 'flex', width: '100%', mt: 4 }}>
+          <Box sx={{ flex: 1, mr: 2 }}>
+            <SecretSantaForm onSubmit={handleFormSubmit} />
+          </Box>
+          <Box sx={{ flex: 1, ml: 2 }}>
+            {pairs.length > 0 && (
+              <>
+                <Typography variant="h5" component="h2" gutterBottom>
+                  Generated Pairs:
+                </Typography>
+                <List>
+                  {pairs.map((pair, index) => (
+                    <ListItem key={index}>
+                      <ListItemText primary={`${pair.giver} gives a gift to ${pair.receiver}`} />
+                    </ListItem>
+                  ))}
+                </List>
+              </>
+            )}
+          </Box>
         </Box>
-        <Box sx={{ flex: 1, ml: 2 }}>
-          {pairs.length > 0 && (
-            <>
-              <Typography variant="h5" component="h2" gutterBottom>
-                Generated Pairs:
-              </Typography>
-              <List>
-                {pairs.map((pair, index) => (
-                  <ListItem key={index}>
-                    <ListItemText primary={`${pair.giver} gives a gift to ${pair.receiver}`} />
-                  </ListItem>
-                ))}
-              </List>
-            </>
-          )}
-        </Box>
-      </Box>
-    </Container>
+      </Container>
+    </>
   );
 }
